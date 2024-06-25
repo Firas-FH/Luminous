826556f538f4156ed9e4369bd5ade096f529ed7d
naya-web <nayaalnakola@gmail.com>
2024-06-25 19:51:11 +0300

    Add variables to tailwind

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SignUp from '../SignUp/SignUp'
 
 
 function HomePage() {
   return (
     <>
+    {/* <SignUp/> */}
     <h1>HomePage</h1>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <Link to={'/signup'}>sign up</Link>
+    {/* <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Link to={'/signup'}>sign up</Link> */}
     </>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primarytxt: 'var(--primary-text-color)',
+        secondarytxt: 'var(--secondary-text-color)',
+        primarybg: 'var(--primary-bg)',
+        secondarybg:'var(--secondary-bg)',
+        bordercolor:'var(--border-color)',
+        headerscolor:'var(--headers-color)',
+        btnbg:'var(--btn-bg)',
+        highlightcolor:'var(--highlight-color)',
+        alertscolor:'var(--alerts-color)'
+      },
+    },
   },
   plugins: [],
+  darkMode : 'class',
 }
 
