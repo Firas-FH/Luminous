3808a0f83a386bff979af2c7e7472144dabdfc9c
Firas-FA <firascanva505@gmail.com>
2024-06-27 04:46:40 -0700

    occasion Category not completed

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import SignUp from '../SignUp/SignUp'
+// import { Link } from 'react-router-dom'
+// import SignUp from '../SignUp/SignUp'
 
 
 function HomePage() {
   return (
     <>
-    {/* <SignUp/> */}
-    <h1>HomePage</h1>
-    {/* <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      {/* <SignUp/> */}
+      <h1>HomePage</h1>
+      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <Link to={'/signup'}>sign up</Link> */}
     </>
   )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 // import App from './App';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RouterProject } from './Router/Router';
 
 
@@ -10,7 +10,7 @@ import { RouterProject } from './Router/Router';
 const router = createBrowserRouter(RouterProject)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
