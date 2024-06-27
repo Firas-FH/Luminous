993e966c472828a57c5a67ea45bb7f914b838fba
naya-web <nayaalnakola@gmail.com>
2024-06-27 15:01:26 +0300

    sign up linking

--- frontend/src/Pages/SignUp/SignUp.jsx
+++ frontend/src/Pages/SignUp/SignUp.jsx
@@ -1,16 +1,38 @@
 import React, { useState } from 'react'
+
 import './SignUpStyle.css'
-import ballon1 from '../../assets/ballon1.png'
-import ballon2 from '../../assets/ballon2.png'
+// import ballon1 from '../../../public/Picture/signup images/'
+// import ballon2 from './public/ballon2.png'
 // import img from '../../assets/radiant-cosmic-burst-stockcake 2.png'
 
 function SignUp() {
 
+
+  const [name, setUserName] = useState("");
+  const [email, setUserEmail] = useState("");
+  const [password, setUserPassword] = useState("");
+  const [responseMessage, setErrorMessage] = useState("");
+  // const navigate = useNavigate();
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    alert('ddd')
+
+    try {
+      
+
+    
+    } catch (error) {
+    }
+  }
+
+
+
   return (
     <>
     <div className='signup relative flex justify-center items-center mt-20' style={{}}>
-      <img src={ballon1} className='absolute left-20 top-72 w-[225px] h-[225px]' alt="" />
-      <form action="" className='signup-form relative w-1/2 flex justify-center align-middle border border-bordercolor rounded-[36px]'>
+      <img src='./Picture/signup images/ballon1.png' className='absolute left-20 top-72 w-[225px] h-[225px]' alt="" />
+      <form action="" onSubmit={handleSignUp} className='signup-form relative w-1/2 flex justify-center align-middle border border-bordercolor rounded-[36px]'>
       <div className='overlay absolute w-full h-full rounded-[36px] ' ></div>
         <div className='w-2/3 py-8 z-[1]'>
           <p className='text-headerscolor pb-[20px]' style={{fontSize:'28px'}}>Create Account</p>
@@ -18,7 +40,7 @@ function SignUp() {
           <input type="text" className='block w-full border border-bordercolor mt-[5px] mb-[20px] py-[7px] px-[15px] focus:outline-none focus:shadow-inner focus:shadow-headerscolor text-secondarytxt'/>
           <label htmlFor="" className='text-primarytxt text-base '>Email Address</label>
           <div>
-            <input type="email" className='block w-full border border-bordercolor mt-[5px] mb-[20px] py-[7px] px-[15px] focus:outline-none focus:shadow-inner focus:shadow-headerscolor text-secondarytxt'/>
+            <input type="text" className='block w-full border border-bordercolor mt-[5px] mb-[20px] py-[7px] px-[15px] focus:outline-none focus:shadow-inner focus:shadow-headerscolor text-secondarytxt'/>
             <img src="" alt="" />
           </div>
           <label htmlFor="" className='text-primarytxt text-base '>Password</label>
@@ -27,7 +49,7 @@ function SignUp() {
           <p className='text-primarytxt '>Already have an account? <span className='text-headerscolor'>Login</span></p>
         </div>
       </form>
-      <img src={ballon2} className='absolute right-0 bottom-64 w-[157px] h-[157px]' alt="" />
+      <img src='./Picture/signup images/ballon2.png' className='absolute right-0 bottom-64 w-[157px] h-[157px]' alt="" />
     </div>
 
   
