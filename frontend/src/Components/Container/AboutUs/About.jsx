6241bec86e3cd6319719f9a630b9b68b2aad0f7b
Ghazal-Haidar <ghazalhaidar5@gmail.com>
2024-07-02 18:51:49 +0300

    ghazal

--- frontend/src/Components/Container/AboutUs/About.jsx
+++ frontend/src/Components/Container/AboutUs/About.jsx
@@ -1,12 +1,12 @@
 import "./AboutStyle.css"
-import video from './video_2024-06-30_18-23-26.mp4'
+import video from './video.mp4'
 function About() {
     return (
-      <div className="about pl-[9%]  h-screen w-screen">
-        <div className="flex flex-row items-center justify-between">
-          <div className="w-1/3 pt-12 ">
-            <h1 className="text-xl  text-headerscolor mb-4">About Us</h1>
-            <p className="text-2xl text-black mb-8">
+      <div className="about pl-[9%]  h-screen w-screen ">
+<div className=" about1 flex flex-wrap justify-center md:flex-nowrap">
+           <div className="w-full md:w-1/2 xl:w-1/2 pt-12 md:p-6 ">
+            <h1 className="text-xl md:text-3xl lg:text-2xl  text-headerscolor mb-4">About Us</h1>
+           <p className="text-lg md:text-xl lg:text-2xl mb-8">
               Luminous started with a spark of inspiration to illuminate life's special moments. We've grown into a trusted name, known for our creativity, dedication, and passion for bringing people together.
             </p>
             <div className="flex md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8">
@@ -16,10 +16,10 @@ function About() {
               alt="Our Vision"
               className="w-30 h-30 mb-7"
             />
-                <h2 className="text-xl font-bold ">Our Vision:</h2>
-                <p className=" text-base text-black">
-                  We aim to transform events into unforgettable experiences through creativity and dedication, inspiring lasting memories.
-                </p>
+                             <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-2">Our Vision:</h2>
+              <p className="text-base md:text-lg lg:text-xl mb-4">
+              We aim to transform events into unforgettable experiences through creativity and dedication, inspiring lasting memories.
+            </p>
               </div>
               <div >
               <img
@@ -27,34 +27,31 @@ function About() {
               alt="Our Vision"
               className="w-30 h-30 mb-4"
             />
-                <h2 className="text-xl font-bold ">Our Mission:</h2>
-                <p className="text-base text-black">
-                  We create extraordinary events with a personal touch, ensuring perfection in every detail and unforgettable moments.
-                </p>
+               <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-2">Our Mission:</h2>
+              <p className="text-base md:text-lg lg:text-xl mb-4">
+              We create extraordinary events with a personal touch, ensuring perfection in every detail and unforgettable moments.
+            </p>
               </div>
             </div>
             <button className="bg-headerscolor text-white py-2 px-4 text-lg mt-8 hover:bg-highlightcolor transition">Contact Us Now</button>
           </div>
-          <div className="md:w-1/2 pl-24 pt-28">
-            <img src="./Picture/Experience.png" alt="Luminous event planning"  />
+         <div className=" md:w-1/2 xl:w-3/5 pl-[11rem]  md:pt-4 ">
+          <img src="./Picture/Experience.png" alt="Luminous event planning" className=" Experience pt-11   object-cover " />
           </div>
         </div>
-        <div className=" flex justify-center ">
-          <div className="relative  md:w-2/3">
-            {/* <video
-              className="shadow-md h-[28%] pt-14 w-full"
+        <div className=" video flex justify-center pr-[9%] pt-8 ">
+            <video
+              className="shadow-md  w-1/2"
               controls
               autoplay
               muted
-              
             >
               <source src={video} alt="Luminous event planning" type="video/mp4" />
               Your browser does not support the video tag.
-            </video> */}
+            </video>
           </div>
         </div>
-      </div>
     );
   }
   
-  export default About;
+   export default About;
